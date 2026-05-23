090073b6149db902254d7e3837bed4d45e8ec0f8
PBChandler <misterparakeet@gmail.com>
2026-05-23 13:27:01 -0400

    Comedy is now Legal on Squawkr

---
+++ smile/Assets/Game/Scripts/Hacker/Squawkr/SquawkrLoader.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class SquawkrLoader : MonoBehaviour
+{
+    public Image profilePicture;
+    public TextMeshProUGUI DisplayName, Handle, SquawkContent, Date, Time, Likes, Parrots, ReplyCount;
+
+    public SquawkrProfileScriptableObject profile;
+
+    public void Start()
+    {
+        LoadSquawk(profile.Posts[0]);
+    }
+    public void LoadSquawk(SquawkrPost post)
+    {
+        DisplayName.text = profile.DisplayName;
+        Handle.text = profile.Handle;
+        SquawkContent.text = post.TweetText;
+        Date.text = post.Date + " " + post.Time + "  Sent From: " + post.SentFrom;
+        
+        Likes.text = "Likes:" + post.likeCount+"";
+        Parrots.text = "Parrots:" + post.retweetCount;
+        profilePicture.sprite = profile.profilePicture;
+    }
+
+}
---
+++ smile/Assets/Game/Scripts/Hacker/Squawkr/SquawkrProfileScriptableObject.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/New Squawkr Profile", order = 1)]
+public class SquawkrProfileScriptableObject : ScriptableObject
+{
+    public Sprite profilePicture;
+    public string DisplayName, Handle;
+    public string bio;
+    public List<SquawkrPost> Posts;
+}
+
+[System.Serializable]
+public class SquawkrPost
+{
+    public string TweetText;
+    public string Date;
+    public string Time;
+    public string SentFrom;
+
+    public float likeCount;
+    public float retweetCount;
+
+    [Header("Reply Only Variant")]
+
+    public List<SquawkrReply> replies;
+}
+
+[System.Serializable]
+public class SquawkrReply
+{
+    public SquawkrProfileScriptableObject profile;
+    public SquawkrPost replyContext;
+}
