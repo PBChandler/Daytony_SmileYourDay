148c6a5ddd50902f296c750c6ff5fa2def516ef0
PBChandler <misterparakeet@gmail.com>
2026-05-29 21:45:55 -0400

    ARE YOU KIDDING ME?

--- smile/Assets/Game/Scripts/Core/SmileYourDaySteam.cs
+++ smile/Assets/Game/Scripts/Core/SmileYourDaySteam.cs
@@ -9,6 +9,13 @@ public class SmileYourDaySteam : MonoBehaviour
     public Callback<Steamworks.LobbyChatUpdate_t> OnLobbyEntered;
     public Callback<Steamworks.GameOverlayActivated_t> chat;
     
+    public Callback<Steamworks.GameLobbyJoinRequested_t> LetMeIn;
+    public Callback<Steamworks.LobbyMatchList_t>MatchListChanged;
+    public Callback<Steamworks.LobbyEnter_t>LobbyingLikeCongress;
+    public Callback<Steamworks.LobbyDataUpdate_t>DATAUPDATE;
+
+    public Callback<Steamworks.LobbyChatUpdate_t>CHAT;
+    
     void Start()
     {
         PostToEveryone_dg += SYD_Missive_Dummy;
@@ -21,6 +28,12 @@ public class SmileYourDaySteam : MonoBehaviour
         Debug.Log("KENDRICK");
         OnLobbyEntered = Callback<LobbyChatUpdate_t>.Create(OnLobbyMemberJoined);
         chat = Callback<Steamworks.GameOverlayActivated_t>.Create(Krist);
+        //OnLobbyCreatede = Callback<Steamworks.LobbyCreated_t>.Create(OnLobbyCreated);
+         LetMeIn = Callback<Steamworks.GameLobbyJoinRequested_t>.Create(LetMeInReceiver);
+         MatchListChanged = Callback<Steamworks.LobbyMatchList_t>.Create(MatchListChangedReceiver);
+         LobbyingLikeCongress = Callback<Steamworks.LobbyEnter_t>.Create(LobbyingLikeCongressReceiver);
+         DATAUPDATE = Callback<Steamworks.LobbyDataUpdate_t>.Create(DATAUPDATEReceiver);
+         CHAT = Callback<Steamworks.LobbyChatUpdate_t>.Create(CHATReciever);
      }
 
      public void Krist(Steamworks.GameOverlayActivated_t bergentruck)
@@ -37,8 +50,38 @@ public class SmileYourDaySteam : MonoBehaviour
     public void Update()
     {
         SteamAPI.RunCallbacks();
+        if(!SteamAPI.Init())
+        {
+            Debug.Log("STEAM API IS NOT ACTIVE, STOP EVERYTHINGG!!!!!");
+        }
     }
 
+    public void DATAUPDATEReceiver(Steamworks.LobbyDataUpdate_t kerk)
+    {
+        Debug.Log("PLEASE GOD WILL SOMEONE WORK WILL ONE OF THESE WORK!!!!");
+    }
+
+    public void CHATReciever(Steamworks.LobbyChatUpdate_t carolknight)
+    {
+        Debug.Log("Carol"+carolknight.m_ulSteamIDLobby);
+    }
+    /// <summary>
+    /// understanding
+    /// </summary>
+    /// <param name="burger"></param>
+    public void LetMeInReceiver(Steamworks.GameLobbyJoinRequested_t burger)
+    {
+        Debug.Log("I'm trying to break into" + SteamFriends.GetFriendPersonaName(burger.m_steamIDFriend) + "'s lobby");
+    }
+
+    public void LobbyingLikeCongressReceiver(Steamworks.LobbyEnter_t benisherenow)
+    {
+        Debug.Log("HELLO? HIIII??? HELLO??? IS SOMEONE JOINING THE FUCKING LOBBY???" + benisherenow.m_EChatRoomEnterResponse);
+    }
+    public void MatchListChangedReceiver(Steamworks.LobbyMatchList_t benisherenow)
+    {
+        Debug.Log("matchlist updated smile" + benisherenow.m_nLobbiesMatching);
+    }
     public void OnLobbyMemberJoined(LobbyChatUpdate_t pCallback)
     {
         Debug.Log("WE ARE KENDRICK LAMARRRRRRR");
