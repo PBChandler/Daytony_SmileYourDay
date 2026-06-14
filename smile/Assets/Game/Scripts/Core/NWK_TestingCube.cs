2227d281589036582686eaedb419f3e84bdb2443
PBChandler <misterparakeet@gmail.com>
2026-06-14 18:59:45 -0400

    but like what if this just worked

---
+++ smile/Assets/Game/Scripts/Core/NWK_TestingCube.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class NWK_TestingCube : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(10, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-10, 0, 0);
+        }
+    }
+}
