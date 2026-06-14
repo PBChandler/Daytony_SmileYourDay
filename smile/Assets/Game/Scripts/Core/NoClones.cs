59f6ac5e2fd7c9bd1f922973b0dded6bd08758aa
PBChandler <misterparakeet@gmail.com>
2026-06-14 18:55:27 -0400

    shutting up errors

---
+++ smile/Assets/Game/Scripts/Core/NoClones.cs
@@ -0,0 +1,20 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class NoClones : NetworkBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsOwner)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
