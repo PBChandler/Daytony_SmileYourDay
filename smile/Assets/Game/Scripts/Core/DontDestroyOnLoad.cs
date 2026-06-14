50a3ec4abf9f41b71abc0385a1a99e772d8636e5
PBChandler <misterparakeet@gmail.com>
2026-06-14 18:50:24 -0400

    janky solution

--- smile/Assets/Game/Scripts/Core/DontDestroyOnLoad.cs
+++ smile/Assets/Game/Scripts/Core/DontDestroyOnLoad.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
-    void Awake()
+    void OnEnable()
     {
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("TitleScreen", UnityEngine.SceneManagement.LoadSceneMode.Additive);
+        }
     }
 }
