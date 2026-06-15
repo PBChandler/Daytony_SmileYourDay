5db78e820fd7c457d101a02e41959dc58ebbceb9
PBChandler <misterparakeet@gmail.com>
2026-06-14 20:22:42 -0400

    heartbeat theory crafting version 2

--- smile/Assets/Game/Scripts/Core/NWK_Inviter.cs
+++ smile/Assets/Game/Scripts/Core/NWK_Inviter.cs
@@ -10,4 +10,15 @@ public class NWK_Inviter : NetworkBehaviour
        // SteamMatchmaking.CreateLobbyAsync(100);
         SteamManager.Instance.CreateLobby(22);
     }
+    public void FixedUpdate()
+    {
+        HeartbeatRpc();   
+    }
+
+    [Rpc(SendTo.Everyone, InvokePermission = RpcInvokePermission.Everyone)]
+    public void HeartbeatRpc()
+    {
+        if(!IsOwner)
+            Debug.Log("Bah-Bump");
+    }
 }
