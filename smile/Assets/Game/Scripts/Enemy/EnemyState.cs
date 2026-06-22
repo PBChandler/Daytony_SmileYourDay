e8006d2d09a07f4681e4cfe5a6b219dc60b11051
Joshua Shuman <joshua.shuman@snhu.edu>
2026-06-22 15:27:12 -0400

    stuff actually works now
    - enemy now properly moves between states when they need to
    - added new "Alarmed" state for when you pick the wrong answer
    - not fully featured yet but a step in the right direction

--- smile/Assets/Game/Scripts/Enemy/EnemyState.cs
+++ smile/Assets/Game/Scripts/Enemy/EnemyState.cs
@@ -7,11 +7,33 @@ public abstract class EnemyState : MonoBehaviour
     protected int stateTimer;
     protected NavMeshAgent agent;
     protected GameObject runnerRef;
+    protected SphereCollider talk;
+
+    protected EnemyBehavior enemyB;
+    protected int suspicionLevel
+    {
+        get => enemyB.suspicionLevel;
+        set { enemyB.suspicionLevel = value; }
+    }
 
     private void Awake()
     {
         machine = GetComponent<EnemyStateMachine>();
         agent = GetComponent<NavMeshAgent>();
+        enemyB = GetComponent<EnemyBehavior>();
+        // will eventually be simplified when we have a clearer distinction between runner and hacker
+        foreach (GameObject o in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if (runnerRef == null)
+            {
+                runnerRef = o;
+                continue;
+            }
+            if (Vector3.Distance(transform.position, o.transform.position) < Vector3.Distance(transform.position, runnerRef.transform.position))
+                runnerRef = o;
+        }
+
+
     }
     public abstract void OnEnterState();
 
--- smile/Assets/Game/Scripts/Enemy/EnemyVision.cs
+++ smile/Assets/Game/Scripts/Enemy/EnemyVision.cs
@@ -1,21 +1,46 @@
 using System.Collections;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class EnemyVision : MonoBehaviour
 {
     [SerializeField] LayerMask sightMask;
     EnemyBehavior behavior;
+    EnemyStateMachine machine;
+    Alarmed alr;
     FirstPersonController fpc;
     Ray sightCast;
+    bool canSee = false;
+    bool playerInView = false;
 
     public void Awake()
     {
         behavior = transform.parent.GetComponent<EnemyBehavior>();
+        machine = transform.parent.GetComponent<EnemyStateMachine>();
+        alr = transform.parent.GetComponent<Alarmed>();
+    }
+
+    public IEnumerator SightCooldown(float sec)
+    {
+        canSee = false;
+        yield return new WaitForSeconds(sec);
+        canSee = true;
+    }
+
+    public void ToggleSight() => canSee = !canSee;
+    public void SetSight(bool val) => canSee = val;
+
+    private void FixedUpdate()
+    {
+        if (!playerInView && machine.currentState == machine.GetStateFromName("Alarmed"))
+            alr.DecreaseStateTime();
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.tag != "Player")
+            return;
+        if (!canSee)
             return;
 
         fpc = other.transform.parent.GetComponent<FirstPersonController>();
@@ -24,12 +49,20 @@ public class EnemyVision : MonoBehaviour
         sightCast.direction = (other.transform.position - sightCast.origin).normalized;
         if (Physics.Raycast(sightCast, out RaycastHit hit, 8, sightMask))
         {
-            if (hit.collider.tag == "Player")
+            if (hit.collider.tag == "Player" && machine.currentState != machine.GetStateFromName("Alarmed"))
             {
                 if (!fpc.isCrouched) behavior.AddSuspicion(10 - Mathf.FloorToInt(hit.distance), .5f);
 
                 else if (hit.distance < 4) behavior.AddSuspicion(8 - Mathf.FloorToInt(hit.distance), 1);
             }
+            playerInView = true;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Player")
+            return;
+        playerInView = false;
     }
 }
