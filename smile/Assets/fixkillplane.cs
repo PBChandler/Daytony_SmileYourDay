d8d8ae516056c81e6fcaf3e826ed958908c38f0c
PBChandler <prestonbchandler@gmail.com>
2026-08-14 20:24:47 -0400

    bandaider fixer

--- smile/Assets/fixkillplane.cs
+++ smile/Assets/fixkillplane.cs
@@ -4,6 +4,6 @@ public class fixkillplane : MonoBehaviour
 {
     public void OnTriggerEnter(Collider other)
     {
-        other.transform.position += new Vector3(0, 20, 0);
+        other.transform.position = new Vector3(0, 20, 0);
     }
 }
